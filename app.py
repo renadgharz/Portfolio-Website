ac13c74abb3cbc337e4747b82535255fbba0c4f9
Renad <renad.gharz@outlook.com>
2022-06-06 10:54:47 -0600

    loading template pages into app.py

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-	return "<h1>HOME PAGE</h1>"
+	return render_template("index.html")
+
+
+@app.route("/projects")
+def projects():
+	return  render_template("projects.html")
+
+
+@app.route("/skills")
+def skills():
+	return render_template("skills.html")
+
+
+@app.route("/experience")
+def experience():
+	return render_template("experience.html")
+
+
+@app.route("/education")
+def education():
+	return render_template("education/html")
 
 
 if __name__ == "__main__":
-	app.run()
+	app.run(debug=True)
